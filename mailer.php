35344cd0cf68eacb64704be1aedd39b3567f207a
karima <karima.lafria@gmail.com>
2019-10-24 18:07:01 +0200

    gestion email

--- mailer.php
+++ mailer.php
@@ -8,11 +8,6 @@ if(isset($_POST['email']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
 
     $mail = new PHPMailer();
 
-    $mail->setFrom($_POST['email'], $_POST['nom']);
-    $mail->addAddress('karima.lafria@gmail.com', 'karima');
-    $mail->Subject = 'Email de mon site web';
-    $mail->Body    = $_POST['message'];
-
     $mail->isSMTP();
     $mail->HOST = 'smtp.gmail.com';
     $mail->SMTPAuth = true;
@@ -20,6 +15,11 @@ if(isset($_POST['email']) && filter_var($_POST['email'], FILTER_VALIDATE_EMAIL))
     $mail->Username   = 'karima.lafria@gmail.com';
     $mail->Password   = 'mouzoune26';
     $mail->Port       = 587;
+
+    $mail->setFrom($_POST['email'], $_POST['nom']);
+    $mail->addAddress('karima.lafria@gmail.com', 'karima');
+    $mail->Subject = 'Email de mon site web';
+    $mail->Body    = $_POST['message'];
     $mail->send();
 
     if (!$mail->send()) {
